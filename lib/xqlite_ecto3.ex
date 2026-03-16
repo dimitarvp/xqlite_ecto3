535d7985d8e71a4ca042dba5e5dec893c9b866ca
Dimitar Panayotov <mitko.p@gmail.com>
2026-03-16 20:59:08 +0200

    initial commit

---
+++ lib/xqlite_ecto3.ex
@@ -0,0 +1,18 @@
+defmodule XqliteEcto3 do
+  @moduledoc """
+  Documentation for `XqliteEcto3`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> XqliteEcto3.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/xqlite_ecto3_test.exs
@@ -0,0 +1,8 @@
+defmodule XqliteEcto3Test do
+  use ExUnit.Case
+  doctest XqliteEcto3
+
+  test "greets the world" do
+    assert XqliteEcto3.hello() == :world
+  end
+end
